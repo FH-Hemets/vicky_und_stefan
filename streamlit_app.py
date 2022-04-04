9466079a720ccb95cfa56ba79a97a2d9d46c97dd
Stefan Hemetsberger <shemetsberger.itsb-b2017@fh-salzburg.ac.at>
2022-04-04 21:10:00 +0200

    test upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+photo = st.camera_input("Foto machen")
+file = st.file_uploader('Foto hochladen')
